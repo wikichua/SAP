8f9530e162407b3d1e16846c097f706c56cd54ff
Wiki Chua <wikichua@gmail.com>
2020-08-06 14:23:11 +0800

    tidy up

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,6 @@
 require("./bootstrap");
 import route from "ziggy";
-import {
-    Ziggy
-} from "./ziggy";
+import { Ziggy } from './ziggy';
 require('bootstrap-select');
 
 window.moment = require('moment');
