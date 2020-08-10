ed42b5a98b78dc8edb6d62d732b1f9e2c89b3a23
Wiki Chua <wikichua@gmail.com>
2020-08-10 14:19:44 +0800

    permission gate

--- resources/views/sap/components/menus/admin.blade.php
+++ resources/views/sap/components/menus/admin.blade.php
@@ -13,11 +13,21 @@
     <div id="{{ $id }}" class="collapse {{ $groupActive? 'show':'' }}" aria-labelledby="{{ $id }}" data-parent="#accordionSidebar">
         <div class="bg-white py-2 collapse-inner rounded">
             {{-- <h6 class="collapse-header">Custom Components:</h6> --}}
+            @can('Read Users')
             <x-sap-menu-item :href="route('user.list')" active-pattern="user.*">User</x-sap-menu-item>
+            @endcan
+            @can('Read Roles')
             <x-sap-menu-item :href="route('role.list')" active-pattern="role.*">Role</x-sap-menu-item>
+            @endcan
+            @can('Read Permissions')
             <x-sap-menu-item :href="route('permission.list')" active-pattern="permission.*">Permission</x-sap-menu-item>
+            @endcan
+            @can('Read Settings')
             <x-sap-menu-item :href="route('setting.list')" active-pattern="setting.*">Setting</x-sap-menu-item>
+            @endcan
+            @can('Read Activity Logs')
             <x-sap-menu-item :href="route('activity_log.list')" active-pattern="activity_log.*">Activity Log</x-sap-menu-item>
+            @endcan
         </div>
     </div>
 </li>
