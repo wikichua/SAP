8f7e3a6190f52546eafd64eb6ad9e1c35f3cafb8
Wiki Chua <wikichua@gmail.com>
2020-08-19 18:27:06 +0800

    bug fix

--- resources/views/sap/admin/brand/show.blade.php
+++ resources/views/sap/admin/brand/show.blade.php
@@ -4,22 +4,16 @@
     <div class="card-header py-3">
         <div class="btn-toolbar justify-content-between" role="toolbar">
             <div class="btn-group" role="group">
-                <a href="{{ route('permission.list') }}" class="btn btn-link">
+                <a href="{{ route('brand.list') }}" class="btn btn-link">
                 <i class="fas fa-angle-double-left mr-2"></i></a>
-                <h3 class="m-0 font-weight-bold text-primary">Show Permission</h3>
+                <h3 class="m-0 font-weight-bold text-primary">Show Brand</h3>
             </div>
             <div class="btn-group" role="group">
             </div>
         </div>
     </div>
-    <div class="card-body">
-        <div class="table-responsive">
-            @csrf
-            <x-sap-display-field type="text" name="name" id="name" label="Brand Name" :value="$model->name" type=""/>
-                <x-sap-display-field type="text" name="published_at" id="published_at" label="Published Date" :value="$model->published_at" type=""/>
-                <x-sap-display-field type="text" name="expired_at" id="expired_at" label="Expired Date" :value="$model->expired_at" type=""/>
-                <x-sap-display-field type="text" name="status" id="status" label="Status" :value="$model->status" type=""/>
-        </div>
+    <div class="embed-responsive embed-responsive-16by9">
+        <iframe src="//{{ $model->domain }}" class="embed-responsive-item"></iframe>
     </div>
 </div>
 @endsection
