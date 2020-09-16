444300b37d14a120c0a140bc3a85d75260645750
Wiki Chua <wikichua@gmail.com>
2020-09-16 18:05:48 +0800

    support laravel 8

--- src/routers/dashboardRoutes.php
+++ src/routers/dashboardRoutes.php
@@ -3,9 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => config('sap.custom_admin_path'),'middleware' => ['web', 'auth_admin', 'can:Access Admin Panel']], function () {
-    Route::group(['prefix' => 'laravel-filemanager'], function () {
-        \UniSharp\LaravelFilemanager\Lfm::routes();
-    });
+    if (class_exists('\UniSharp\LaravelFilemanager\Lfm')) {
+        Route::group(['prefix' => 'laravel-filemanager'], function () {
+            \UniSharp\LaravelFilemanager\Lfm::routes();
+        });
+    }
 
     Route::group(['prefix' => '', 'namespace' => config('sap.controller_namespace') . '\Admin'], function () {
         Route::match(['get', 'head'], '/', 'DashboardController@index')->name('admin');
