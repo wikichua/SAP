f4c25dd84237bf09dc57391144007ec277412a50
Wiki Chua <wikichua@gmail.com>
2020-10-08 19:19:05 +0800

    upda

---
+++ stubs/brand/config/main.php
@@ -0,0 +1,5 @@
+<?php
+return [
+    'resources_path' => base_path('brand/{%brand_string%}/resources/views'),
+    'template_path' => base_path('brand/{%brand_string%}/resources/views/layouts'),
+];
