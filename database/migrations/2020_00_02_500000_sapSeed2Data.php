e87b30994514d9a6c70a50b483ef08f1b644b3c7
Wikichua <wikichua@gmail.com>
2021-01-23 20:53:05 +0800

    up

--- database/migrations/2020_00_02_500000_sapSeed2Data.php
+++ database/migrations/2020_00_02_500000_sapSeed2Data.php
@@ -10,9 +10,10 @@ class SapSeed2Data extends Migration
     {
         $admin_id = 1;
         app(config('sap.models.permission'))->createGroup('Wiki Docs', ['Read Wiki Docs'], $admin_id);
+        app(config('sap.models.permission'))->createGroup('SEO Manager', ['Manage SEO'], $admin_id);
     }
     public function down()
     {
-        app(config('sap.models.permission'))->whereIn('group', ['Wiki Docs'])->delete();
+        app(config('sap.models.permission'))->whereIn('group', ['Wiki Docs', 'SEO Manager'])->delete();
     }
 }
--- resources/views/sap/components/admin-menu.blade.php
+++ resources/views/sap/components/admin-menu.blade.php
@@ -23,10 +23,14 @@
 <x-sap::menu menu="menu" label="File Manager" :route="route('lfm.home')" icon="fas fa-fw fa-folder" :active-patterns="['lfm.*']"/>
 @endif
 @if (Route::has('seo.home'))
+@can('Manage SEO')
 <x-sap::menu menu="menu" label="SEO Manager" :route="route('seo.home')" icon="fas fa-fw fa-folder" :active-patterns="['seo.*']"/>
+@endcan
 @endif
 @if (Route::has('wiki.home'))
+@can('Read Wiki Docs')
 <x-sap::menu menu="menu" label="Wiki Docs" :route="route('wiki.home')" icon="fab fa-fw fa-wikipedia-w" :active-patterns="['wiki.*']"/>
+@endcan
 @endif
 <!--DoNotRemoveMe-->
 @foreach ($brandMenus as $brandMenu)
