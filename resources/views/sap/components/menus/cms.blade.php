11f04b60f24fd39bbc5c607ca6890418abf75563
Your Name <you@example.com>
2021-03-17 17:12:26 +0800

    up

--- resources/views/sap/components/menus/cms.blade.php
+++ resources/views/sap/components/menus/cms.blade.php
@@ -23,25 +23,25 @@
         <div class="bg-white py-2 collapse-inner rounded">
             {{-- <h6 class="collapse-header">Custom Components:</h6> --}}
             @can('Read Brands')
-            <x-sap::menu-item :href="route('brand.list')" :active-pattern="'brand.*'" icon="fas fa-laptop">Brand</x-sap-menu-item>
+            <x-sap::menu-item :href="route('brand.list')" :active-pattern="'brand.*'" icon="fas fa-laptop">Brand</x-sap::menu-item>
             @endcan
             @can('Read Pages')
-            <x-sap::menu-item :href="route('page.list')" :active-pattern="'page.*'" icon="fas fa-blog">Page</x-sap-menu-item>
+            <x-sap::menu-item :href="route('page.list')" :active-pattern="'page.*'" icon="fas fa-blog">Page</x-sap::menu-item>
             @endcan
             @can('Read Navs')
-            <x-sap::menu-item :href="route('nav.list')" :active-pattern="'nav.*'" icon="fas fa-bars">Nav</x-sap-menu-item>
+            <x-sap::menu-item :href="route('nav.list')" :active-pattern="'nav.*'" icon="fas fa-bars">Nav</x-sap::menu-item>
             @endcan
             @can('Read Components')
-            <x-sap::menu-item :href="route('component.list')" :active-pattern="'component.*'" icon="fas fa-cube">Component</x-sap-menu-item>
+            <x-sap::menu-item :href="route('component.list')" :active-pattern="'component.*'" icon="fas fa-cube">Component</x-sap::menu-item>
             @endcan
             @can('Read Carousels')
-            <x-sap::menu-item :href="route('carousel.list')" :active-pattern="'carousel.*'" icon="fas fa-images">Carousel</x-sap-menu-item>
+            <x-sap::menu-item :href="route('carousel.list')" :active-pattern="'carousel.*'" icon="fas fa-images">Carousel</x-sap::menu-item>
             @endcan
             @can('Read Files')
-            <x-sap::menu-item :href="route('file.list')" :active-pattern="'file.*'" icon="fas fa-archive">Files</x-sap-menu-item>
+            <x-sap::menu-item :href="route('file.list')" :active-pattern="'file.*'" icon="fas fa-archive">Files</x-sap::menu-item>
             @endcan
             @can('Read Pushers')
-            <x-sap::menu-item :href="route('pusher.list')" :active-pattern="'pusher.*'" icon="fas fa-envelope-open-text">Pusher</x-sap-menu-item>
+            <x-sap::menu-item :href="route('pusher.list')" :active-pattern="'pusher.*'" icon="fas fa-envelope-open-text">Pusher</x-sap::menu-item>
             @endcan
         </div>
     </div>
